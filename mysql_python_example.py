489a339a9438bea6ec946d17c0f19f85c0e66920
lshlsh135@naver.com <lshlsh135@naver.com>
2017-04-24 11:30:03 +0900

    20170424 오전 업무

---
+++ mysql_python_example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 08:05:00 2017
+
+@author: SH-NoteBook
+"""
+
+import mysql.connector
+mysql.connector.connect(host='localhost',database='mysql',user='root',password='22tkdgns@@')
