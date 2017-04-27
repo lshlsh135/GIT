c516d5b2ab3a59c2f7870b015ec8862742455283
lshlsh135@naver.com <lshlsh135@naver.com>
2017-04-27 15:12:28 +0900

    20170427 마무리

--- Gross_Performance/KOSPI200.py
+++ Gross_Performance/KOSPI200.py
@@ -4,7 +4,9 @@ Created on Wed Apr 26 16:36:51 2017
 
 @author: SH-NoteBook
 """
-
+#==============================================================================
+#KOSPI200 
+#==============================================================================
 import numpy as np
 import pandas as pd
 result_operation = pd.DataFrame(np.zeros((1,15)))
@@ -12,6 +14,7 @@ result_net_income = pd.DataFrame(np.zeros((1,15)))
 raw_data = pd.read_pickle('raw_data')
 operation = pd.read_pickle('operation')
 #net_income = pd.read_excel('KOSPI200.xlsm',sheetname='당기순이익',header=None)
+raw_data_size = pd.read_pickle('raw_data_size')
 net_income = pd.read_pickle('net_income')
 
 for i in range(0,15):
@@ -25,21 +28,87 @@ for i in range(0,15):
     data = pd.concat([data,net_income[i+3]],axis=1,join='inner',ignore_index=True)
     data=data[data[18].notnull()]
     result_net_income.loc[0,i]=np.sum(data[18])
-
+    
 #==============================================================================
-# 적자기업 제외
+# 코스피 대중소
 #==============================================================================
 import numpy as np
 import pandas as pd
-result = pd.DataFrame(np.zeros((1,15)))
+result_operation = pd.DataFrame(np.zeros((1,15)))
+result_net_income = pd.DataFrame(np.zeros((1,15)))
 raw_data = pd.read_pickle('raw_data')
 operation = pd.read_pickle('operation')
-for i in range(0,15):
-    
+#net_income = pd.read_excel('KOSPI200.xlsm',sheetname='당기순이익',header=None)
+raw_data_size = pd.read_pickle('raw_data_size')
+net_income = pd.read_pickle('net_income')
 
+for i in range(0,15):
+    data = raw_data_size[(raw_data_size[i+3]==2)|(raw_data_size[i+3]==3)]
+    data = pd.concat([data,operation[i+3]],axis=1,join='inner',ignore_index=True)
+    data=data[data[18].notnull()]
+    result_operation.loc[0,i]=np.sum(data[18])
+    
+for i in range(0,15):
+    data = raw_data_size[(raw_data_size[i+3]==2)|(raw_data_size[i+3]==3)]
+    data = pd.concat([data,net_income[i+3]],axis=1,join='inner',ignore_index=True)
+    data=data[data[18].notnull()]
+    result_net_income.loc[0,i]=np.sum(data[18])
+
+
+
+
+#==============================================================================
+# KOSPI200 적자기업 제외
+#==============================================================================
+import numpy as np
+import pandas as pd
+result_operation = pd.DataFrame(np.zeros((1,15)))
+result_net_income = pd.DataFrame(np.zeros((1,15)))
+raw_data = pd.read_pickle('raw_data')
+operation = pd.read_pickle('operation')
+#net_income = pd.read_excel('KOSPI200.xlsm',sheetname='당기순이익',header=None)
+raw_data_size = pd.read_pickle('raw_data_size')
+net_income = pd.read_pickle('net_income')
+
+for i in range(0,15):
     data = raw_data[raw_data[i+3]==1]
     data = pd.concat([data,operation[i+3]],axis=1,join='inner',ignore_index=True)
     data=data[data[18].notnull()]
     data=data[data[18]>0]
-    result.loc[0,i]=np.sum(data[18])
+    result_operation.loc[0,i]=np.sum(data[18])
+    
+for i in range(0,15):
+    data = raw_data[raw_data[i+3]==1]
+    data = pd.concat([data,net_income[i+3]],axis=1,join='inner',ignore_index=True)
+    data=data[data[18].notnull()]
+    data=data[data[18]>0]
+    result_net_income.loc[0,i]=np.sum(data[18])
+    
+#==============================================================================
+# 코스피 대중소 적자기업 제
+#==============================================================================
+import numpy as np
+import pandas as pd
+result_operation = pd.DataFrame(np.zeros((1,15)))
+result_net_income = pd.DataFrame(np.zeros((1,15)))
+raw_data = pd.read_pickle('raw_data')
+operation = pd.read_pickle('operation')
+#net_income = pd.read_excel('KOSPI200.xlsm',sheetname='당기순이익',header=None)
+raw_data_size = pd.read_pickle('raw_data_size')
+net_income = pd.read_pickle('net_income')
+
+for i in range(0,15):
+    data = raw_data_size[(raw_data_size[i+3]==2)|(raw_data_size[i+3]==3)]
+    data = pd.concat([data,operation[i+3]],axis=1,join='inner',ignore_index=True)
+    data=data[data[18].notnull()]
+    data=data[data[18]>0]
+    result_operation.loc[0,i]=np.sum(data[18])
+    
+for i in range(0,15):
+    data = raw_data_size[(raw_data_size[i+3]==2)|(raw_data_size[i+3]==3)]
+    data = pd.concat([data,net_income[i+3]],axis=1,join='inner',ignore_index=True)
+    data=data[data[18].notnull()]
+    data=data[data[18]>0]
+    result_net_income.loc[0,i]=np.sum(data[18])
+
 
--- mysql_python_example.py
+++ mysql_python_example.py
@@ -1,9 +1,54 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr 24 08:05:00 2017
-
+https://dev.mysql.com/doc/connector-python/en/connector-python-example-ddl.html
 @author: SH-NoteBook
 """
-
+#==============================================================================
+# http://pythonstudy.xyz/python/article/202-MySQL-%EC%BF%BC%EB%A6%AC
+#==============================================================================
 import mysql.connector
-mysql.connector.connect(host='localhost',database='mysql',user='root',password='22tkdgns@@')
+conn = mysql.connector.connect(host='localhost',database='classicmodels',user='root',password='22tkdgns@@')
+curs = conn.cursor(dictionary=True) # 딕셔너리 뭔가 좋아보임
+curs2 = conn.cursor()
+
+sql = "select * from customers"
+curs.execute(sql)
+curs2.execute(sql)
+
+rows = curs.fetchall()
+rows2 = curs2.fetchall()
+
+print(rows)
+print(rows2)
+conn.close()
+#==============================================================================
+# 
+#==============================================================================
+import mysql.connector
+from mysql.connector import Error
+ 
+ 
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='python_mysql',
+                                       user='root',
+                                       password='22tkdgns@@')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+ 
+    except Error as e:
+        print(e)
+ 
+    finally:
+        conn.close()
+ 
+ 
+if __name__ == '__main__':
+    connect()
+    
+#==============================================================================
+#  
+#==============================================================================
