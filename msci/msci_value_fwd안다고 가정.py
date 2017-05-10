f2ad42ed901dc6685c1391b7312f339f04735e65
lshlsh135@naver.com <lshlsh135@naver.com>
2017-05-10 15:49:05 +0900

    20170510  진짜 마무리

--- msci/msci_value_fwd안다고 가정.py
+++ msci/msci_value_fwd안다고 가정.py
@@ -146,23 +146,26 @@ import numpy as np
 
 raw_data = pd.read_pickle('raw_data')
 #size = pd.read_pickle('size')  #시가총액
-ni = pd.read_pickle('ni')
+ni = pd.read_pickle('ni') # 당기순이익
 rtn = pd.read_pickle('rtn')
-equity = pd.read_pickle('equity')
+equity = pd.read_pickle('equity') #자본총계
 operate = pd.read_pickle('operate')  
 kospi = pd.read_pickle('kospi')
 beta_week = pd.read_pickle('beta_week')
 ni_12fw = pd.read_pickle('ni_12fw')
 cash_div = pd.read_pickle('cash_div')
 #size_FIF=pd.read_pickle('size_FIF')  #자기주식 제외 시가총액
-#size_FIF_insider=pd.read_pickle('size_FIF_insider') #자기주식, 최대주주 주식 제외 시가총
+size_FIF_insider=pd.read_pickle('size_FIF_insider') #자기주식, 최대주주 주식 제외 시가총
+#size_FIF_wisefn = pd.read_excel('msci_rawdata.xlsx',sheetname='유통주식수x수정주가1',header=None) # wisefn에서 산출해주는 유통비율 이용
+#size_FIF_wisefn.to_pickle('size_FIF_wisefn')
 size_FIF_wisefn=pd.read_pickle('size_FIF_wisefn') #시가총액
 
 return_data = np.zeros((5,63))
 return_data = pd.DataFrame(return_data)
+data_name=pd.DataFrame(np.zeros((500,63)))
 for n in range(3,66):
     #65마지막 분기
-    data_big = raw_data[(raw_data[n] == 1)]
+    data_big = raw_data[(raw_data[n] == 1)|(raw_data[n] == 2)]
     data_big = data_big.loc[:,[1,n]]
     data = pd.concat([data_big, size_FIF_wisefn[n], equity[n], ni[n+1],cash_div[n]],axis=1,join='inner',ignore_index=True)
     data.columns = ['name','group','size','equity','ni_12fw','cash_div']
@@ -170,12 +173,15 @@ for n in range(3,66):
     data['1/pbr']=data['equity']/data['size']
     data['1/per']=data['ni_12fw']/data['size']
     data['1/div_yield']=data['size']/data['cash_div']
+    
+    # inf, -inf 값들을 NAN 값으로 변경 (그래야 한번에 제거 가능)
     data = data.replace([np.inf, -np.inf],np.nan)  
-    data=data[data['1/pbr'].notnull()]    # per가 NAN인 Row 제외
-#    data=data[data['1/pbr']>0]
-    data=data[data['1/per'].notnull()]
-    data=data[data['1/div_yield'].notnull()]    # per가 NAN인 Row 제외
-#    data=data[data['1/div_yield']>0]
+    
+    # Null 값 제거
+    data_per = data[data['1/per'].notnull()]
+    data_pbr = data[data['1/pbr'].notnull()]
+    data_div = data[data['1/div_yield'].notnull()]
+
 #####################################################################   
 #    양 끝단 제거 안하고 해본
 #    m_pbr=np.mean(data['1/pbr'])
@@ -193,53 +199,68 @@ for n in range(3,66):
 #    data=data.assign(z_score=data1+data2+data3)
 #    data[data['name']=='삼성전자']
 ######################################################################
-    #양 끝 2.5% 제거
-    market_capital=np.sum(data['size'])
-    pbr_min=np.percentile(data['1/pbr'],2.5)
-    pbr_max=np.percentile(data['1/pbr'],97.5)
-    per_min=np.percentile(data['1/per'],2.5)
-    per_max=np.percentile(data['1/per'],97.5)
-    div_min=np.percentile(data['1/div_yield'],2.5)
-    div_max=np.percentile(data['1/div_yield'],97.5)
+    #양 끝 5% 구함
+#    market_capital=np.sum(data['size'])
+    pbr_min=np.percentile(data_pbr['1/pbr'],2.5)
+    pbr_max=np.percentile(data_pbr['1/pbr'],97.5)
+    per_min=np.percentile(data_per['1/per'],2.5)
+    per_max=np.percentile(data_per['1/per'],97.5)
+    div_min=np.percentile(data_div['1/div_yield'],2.5)
+    div_max=np.percentile(data_div['1/div_yield'],97.5)
+
     
-        
-    data_q=data[(data['1/pbr']>pbr_min)&(data['1/pbr']<pbr_max)
-    &(data['1/per']>per_min)&(data['1/per']<per_max)&(data['1/div_yield']<div_max)
-    &(data['1/div_yield']>div_min)]
-    #시가총액비중 구함 (양끝단  5% 제거)
-    data_q=data_q.assign(market_weight=data_q['size']/market_capital)
-    mu_inv_pbr=np.sum(data_q['1/pbr']*data_q['market_weight'])
-    mu_inv_per=np.sum(data_q['1/per']*data_q['market_weight'])
-    mu_inv_div=np.sum(data_q['1/div_yield']*data_q['market_weight'])
+    #양끝단  5% 제거
+    data_pbr = data_pbr[(data_pbr['1/pbr']>pbr_min)&(data_pbr['1/pbr']<pbr_max)]
+    data_per = data_per[(data_per['1/per']>per_min)&(data_per['1/per']<per_max)]
+    data_div = data_div[(data_div['1/div_yield']>div_min)&(data_div['1/div_yield']<div_max)]
+
+    # 시가총액비중 구함 
+    data_pbr_cap = np.sum(data_pbr['size'])
+    data_per_cap = np.sum(data_per['size'])
+    data_div_cap = np.sum(data_div['size'])
+
+    data_pbr = data_pbr.assign(market_weight=data_pbr['size']/data_pbr_cap)
+    data_per = data_per.assign(market_weight=data_per['size']/data_per_cap)
+    data_div = data_div.assign(market_weight=data_div['size']/data_div_cap)
     
-    std_inv_pbr=np.sqrt(np.square(data_q['1/pbr']-mu_inv_pbr)*data_q['market_weight'])
-    std_inv_per=np.sqrt(np.square(data_q['1/per']-mu_inv_per)*data_q['market_weight'])
-    std_inv_div=np.sqrt(np.square(data_q['1/div_yield']-mu_inv_div)*data_q['market_weight'])
-   
-    data1=(data_q['1/pbr']-mu_inv_pbr)/std_inv_pbr
-    data2=(data_q['1/per']-mu_inv_per)/std_inv_per
-    data3=(data_q['1/div_yield']-mu_inv_div)/std_inv_div
+    # 시총가중 평균 
+    mu_inv_pbr=np.sum(data_pbr['1/pbr']*data_pbr['market_weight'])
+    mu_inv_per=np.sum(data_per['1/per']*data_per['market_weight'])
+    mu_inv_div=np.sum(data_div['1/div_yield']*data_div['market_weight'])
+    
+    # 시총 가중 표준편자
+    std_inv_pbr=np.sqrt(np.sum(np.square(data_pbr['1/pbr']-mu_inv_pbr)*data_pbr['market_weight']))
+    std_inv_per=np.sqrt(np.sum(np.square(data_per['1/per']-mu_inv_per)*data_per['market_weight']))
+    std_inv_div=np.sqrt(np.sum(np.square(data_div['1/div_yield']-mu_inv_div)*data_div['market_weight']))
+    
+    data1=(data_pbr['1/pbr']-mu_inv_pbr)/std_inv_pbr
+    data1.name= 'pbr_z'
+    data2=(data_per['1/per']-mu_inv_per)/std_inv_per
+    data2.name= 'per_z'
+    data3=(data_div['1/div_yield']-mu_inv_div)/std_inv_div
+    data3.name= 'div_z'
           
-    data_q=data_q.assign(z_score=data1+data2+data3)
+    result = pd.concat([data, data1, data2, data3], axis = 1)
     
-    data=data_q[data_q['z_score']>0]
+    # np.nanmean : nan 값 포함해서 평균 내기!!
+    result = result.assign(z_score=np.nanmean(result.iloc[:,[9,10,11]],axis=1))
     
+    # z_score > 0 인것이 가치주라고 msci에서 하고있음
+    result =result[result['z_score']>0]
     
-    #z_score 상위 30%
-    z_max=np.percentile(data['z_score'],70)
-    data=data[(data['z_score']>=z_max)]
-    market_capital=np.sum(data['size'])
-    data=data.assign(market_weight2=data_q['size']/market_capital)
-    
-    
-    
-    
-    data = pd.concat([data,rtn[n-3]],axis=1,join='inner',ignore_index=True)
-    #수익률 매칭
-    return_data.iloc[0,n-3]=np.sum(data[12]*data[11])
+    market_capital=np.sum(result['size'])
+    result=result.assign(market_weight2=result['size']/market_capital)
+    result = pd.concat([result,rtn[n-3]],axis=1,join='inner',ignore_index=True) #수익률 매칭
+    return_data.iloc[0,n-3]=np.mean(result[14])
+    data_name[n-3]=result[0].reset_index(drop=True)
+#    return_data.iloc[0,n-3]=np.sum(result[13]*result[14])    
     if n == 65 : 
         pass
     return_final=np.product(return_data,axis=1)
+
+    # 삼성전자가 몇분기동안 포함되었는지 확인
+np.sum(np.sum(data_name=="삼성전자"))
+
 
 
 #==============================================================================
