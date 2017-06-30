36ebf78809efcb8e403c87c223374c44dd91c93b
lshlsh135@naver.com <lshlsh135@naver.com>
2017-06-30 10:32:06 +0900

    df.columns.get_loc() 이걸로 특정 column이 몇번쨰 column인지 알수있음 해결 개쩜

--- A trend factor/A_trend_factor.py
+++ A trend factor/A_trend_factor.py
@@ -11,6 +11,29 @@ import numpy as np
 
 return_data = np.zeros((5,194))
 return_data = pd.DataFrame(return_data)
-rtn_daily = pd.read_excel('A_trend_factor.xlsm',sheetname='일별주가1')
-rtn_daily.to_pickle('rtn_daily')
+#rtn_daily1 = pd.read_excel('A_trend_factor.xlsm',sheetname='일별주가1',header=None)
+#monthly_date = pd.read_excel('A_trend_factor.xlsm',sheetname='월말날짜1',header=None)
+#rtn_daily1.to_pickle('rtn_daily1')
+monthly_date = pd.read_pickle('monthly_date')
 rtn_daily = pd.read_pickle('rtn_daily')
+rtn_daily1 = pd.read_pickle('rtn_daily1')
+
+
+
+a=rtn_daily.iloc[0,:999]
+monthly_date.iloc[0,0]
+
+
+
+#이거다이거
+rtn_daily.columns.get_loc(monthly_date.iloc[0,0])
+
+
+
+
+
+monthly_date.loc[:,:10]
+monthly_date.index[monthly_date.iloc[0,0]]
+monthly_date.index.duplicated()
+
+rtn_daily1.index.get_loc(monthly_date.iloc[0,0])
